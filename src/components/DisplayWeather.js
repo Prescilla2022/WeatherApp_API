d7eeb1ff5c2f36254288003aaf9112740754734e
prescillaanuop <prescillaanuop@gmail.com>
2022-12-14 13:03:47 +0100

    Configure React app for deployment to GitHub Pages

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/searchWeather.css";
+
+//Function to diplay Api data
 export default function DisplayWeather(props) {
   return (
     <div className="searchweather">
       <h2 className="name">{props.data.name}</h2>
       <h2 class="temp">{Math.round(props.data.main.temp - 273.15)} &#x2103;</h2>
+      <h2 class="feels">
+        Feels like:{Math.round(props.data.main.feels_like - 273.15)} &#x2103;
+      </h2>
       <h2 class="desc">{props.data.weather[0].description}</h2>
-
       <div className="details">
         <h2>Humidity:{props.data.main.humidity}%</h2>
         <h2>Wind:{props.data.wind.speed}m/sec</h2>
--- src/components/SearchWeather.js
+++ src/components/SearchWeather.js
@@ -3,14 +3,19 @@ import DisplayWeather from "./DisplayWeather";
 import "../styles/searchWeather.css";
 
 //Function to fetch data using API
+
+//Set state
 export default function SearchWeather() {
   const [city, setCity] = React.useState("oslo");
   const [weatherData, setWeatherData] = React.useState({});
   const apiKey = "937feab78c001c55c9c7dfd7c14b0f7e";
 
+  //Function to handle the changes in text field
   function handleChange(event) {
     setCity(event.target.value);
   }
+
+  //Function to fetch data using API
   async function getWeatherData() {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
@@ -18,6 +23,7 @@ export default function SearchWeather() {
     setWeatherData(response);
     console.log(weatherData);
   }
+  //Hook to execute the API fetch on page load
   useEffect(() => {
     console.log("running");
     getWeatherData(); // eslint-disable-next-line
